d4cb6dd83366e63a7275c00424c1f907aa3e5a83
Nadim Maraqten <nmaraqten@gmail.com>
2024-05-03 10:30:03 +0200

    chessboard mit accuracy und time

--- Übung 1/src/Uebung1_Nadim_chessboard.m
+++ Übung 1/src/Uebung1_Nadim_chessboard.m
@@ -1,78 +1,144 @@
-clear; close all; clc;
-format longG;
-run init; % Assuming 'init' script setups the environment correctly
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Musterkennung Übung 1
+% Gruppe 1
+% Christian Edelmann 3560916
+% Lars Pfeiffer 
+% Nadim Maraqten
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all; format longG; close all; clc;
+run init;
+%#ok<*NOPTS>
 warning('off', 'Images:initSize:adjustingMag');
 rng(1);
+tic;  % Start timing
 
-%% Parameters
-scale = 0.5;  % Reduction in image size
-segment_size = [10, 10]; % Segment size for chessboard segmentation, adjustable
-r = 2;        % Tile row
-c = 13;       % Tile column
 
-disp('----------------------------');
 
-%% Load and Scale Data
-RGBIR = single(d_RGBIR.loadData(r, c)) / 255;
-RGB = imresize(RGBIR(:,:,1:3), scale, 'nearest');
-IR = imresize(RGBIR(:,:,4), scale, 'nearest');
-nDSM = imresize(single(d_nDSM.loadData(r, c)), scale, 'nearest');
-nDSM = (nDSM - min(nDSM(:))) / (max(nDSM(:)) - min(nDSM(:)));
-gt = imresize(uint8(data.potsdam.rgbLabel2classLabel(d_GT.loadData(r, c))), scale, 'nearest');
 
-% Display initial RGB image
+%% Parameters:
+% Input:
+scale  = 1/2
+r      = 2   % tile row
+c      = 13  % tile column
+segment_size=10
+segments=3000/10
+disp('----------------------------')
+
+
+
+%% Load Data:
+% Load data by row / column number:
+
+RGBIR = single(d_RGBIR.loadData(r, c))/255;
+RGB   = RGBIR(:,:,1:3);
+IR    = RGBIR(:,:,4);
+
+nDSM  = single(d_nDSM.loadData(r, c));
+nDSM  = (nDSM - min(nDSM(:))) / (max(nDSM(:)) - min(nDSM(:)));
+
+gt    = d_GT.loadData(r, c);
+gt    = uint8(data.potsdam.rgbLabel2classLabel(gt));
+
+RGB   = imresize(RGB,  scale,  'method', 'nearest');
+nDSM  = imresize(nDSM, scale,  'method', 'nearest');
+IR    = imresize(IR,   scale,  'method', 'nearest');
+gt    = imresize(gt,   scale,  'method', 'nearest');
+
+
 figure;
 imshow(RGB);
-title(sprintf('Scaled Input RGB image, scale = %f', scale));
+title(sprintf('Input image RGB, scale = %f',scale));
 
-%% Chessboard Segmentation
-num_segments_x = ceil(size(RGB,1) / segment_size(1));
-num_segments_y = ceil(size(RGB,2) / segment_size(2));
-segmentation_mask = zeros(size(RGB,1), size(RGB,2));
+% 
+% figure;
+% imshow(IR);
+% title(sprintf('Input image IR, scale = %f',scale));
+% 
+% figure;
+% imshow(nDSM, []);
+% title(sprintf('Input image nDSM, scale = %f',scale));
+% 
+figure;
+imshow(gt, getColorMap('V2DLabels'));
+title(sprintf('Input labels, scale = %f',scale));
 
-% Prepare lists for feature vectors and labels
-feature_vectors = [];
-labels = [];
 
-for ix = 1:num_segments_x
-    for iy = 1:num_segments_y
-        x_start = (ix-1)*segment_size(1) + 1;
-        y_start = (iy-1)*segment_size(2) + 1;
-        x_end = min(ix*segment_size(1), size(RGB,1));
-        y_end = min(iy*segment_size(2), size(RGB,2));
+%% Chessboard
 
-        segment_RGB = RGB(x_start:x_end, y_start:y_end, :);
-        segment_gt = gt(x_start:x_end, y_start:y_end);
+RGB_R=RGB(:,:,1);
+RGB_G=RGB(:,:,2);
+RGB_B=RGB(:,:,3);
 
-        avg_R = mean(segment_RGB(:,:,1), 'all');
-        avg_G = mean(segment_RGB(:,:,2), 'all');
-        avg_B = mean(segment_RGB(:,:,3), 'all');
-        feature_vector = [avg_R, avg_G, avg_B];
-        feature_vectors = [feature_vectors; feature_vector];
-        
-        segment_label = mode(segment_gt(:));
-        labels = [labels; segment_label];
-        
-        segmentation_mask(x_start:x_end, y_start:y_end) = segment_label;
+
+
+label_image=zeros(3000);
+
+feature_R=ones(3000);
+feature_G=ones(3000);
+feature_B=ones(3000);
+feature_nDSM=ones(3000);
+feature_IR=ones(3000);
+
+for i=1:segments
+    for j=1:segments
+        idx(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=i*j*ones(segment_size);
+        segment_gt_label=mode(gt(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+        label_image(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=segment_gt_label;
+
+        feature_R(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(RGB_R(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+        feature_G(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(RGB_G(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+        feature_B(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(RGB_B(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+        feature_nDSM(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(nDSM(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+        feature_IR(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(IR(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+
     end
 end
+idx=reshape(idx,[],1);
 
-% Display the segmentation mask
-figure;
-imshow(label2rgb(segmentation_mask));
-title('Chessboard Segmentation Mask');
+mask=reshape(idx,3000,3000);
 
-% Optionally, overlay the segment borders on the RGB image
-edges = edge(segmentation_mask, 'Sobel'); % Detect edges
-se = strel('disk', 1);  % Create a structuring element for dilation
-edges_dilated = imdilate(edges, se);  % Dilate edges
+figure
+imshow(mask,[])
+title(['chessboard segmentation mask with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
 
-figure;
-imshow(imoverlay(RGB, edges_dilated, 'w'));
-title('Enhanced Segment Borders on RGB Image');
+figure
+bmask = boundarymask(mask);
+imshow(imoverlay(RGB,bmask,'cyan'))
+title(['chessboard segments on RGB image with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
 
-%% Output results
-disp('Feature vectors per segment:');
-disp(feature_vectors);
-disp('Labels per segment:');
-disp(labels);
+imp_surf = label_image == 1;
+building = label_image == 2;
+low_veg  = label_image == 3;
+tree     = label_image == 4;
+car      = label_image == 5;
+clutter  = label_image == 6;
+
+r = imp_surf | car | clutter;
+g = imp_surf | low_veg | tree | car;
+b = imp_surf | building | low_veg;
+
+RGB_label_image = cat(3,r,g,b) * 255;
+
+feature_RGB=ones(3000,3000,3);
+feature_RGB(:,:,1)=feature_R;
+feature_RGB(:,:,2)=feature_G;
+feature_RGB(:,:,3)=feature_B;
+
+figure
+imshow(feature_RGB)
+title(['chessboard RGB features with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
+
+figure
+imshow(RGB_label_image)
+title(['chessboard labeled image with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
+
+%% determine accuracy
+result_label_assignments = 1*imp_surf + 2*building + 3*low_veg + 4*tree + 5*car + 6*clutter;
+correctly_identified_labels = result_label_assignments == gt;
+accuracy = sum(correctly_identified_labels, "all") / (3000*3000)
+
+%% determine runtime
+elapsedTime = toc;  % End timing and capture the elapsed time
+disp(['Elapsed Time: ', num2str(elapsedTime), ' seconds']);
+
