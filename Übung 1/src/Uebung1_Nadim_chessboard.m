03c143cf9cf8dcec01d7d294f0c891b69080f840
Nadim Maraqten <nmaraqten@gmail.com>
2024-05-03 11:12:21 +0200

    chessboard ready

--- Übung 1/src/Uebung1_Nadim_chessboard.m
+++ Übung 1/src/Uebung1_Nadim_chessboard.m
@@ -21,11 +21,12 @@ tic;  % Start timing
 scale  = 1/2
 r      = 2   % tile row
 c      = 13  % tile column
-segment_size=10
-segments=3000/10
+segments_vector = [10, 20, 30, 40, 50, 100, 200, 300, 400, 500];
 disp('----------------------------')
 
-
+% Preallocation for performance
+accuracies = zeros(length(segments_vector), 1);
+times = zeros(length(segments_vector), 1);
 
 %% Load Data:
 % Load data by row / column number:
@@ -80,65 +81,116 @@ feature_B=ones(3000);
 feature_nDSM=ones(3000);
 feature_IR=ones(3000);
 
-for i=1:segments
-    for j=1:segments
-        idx(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=i*j*ones(segment_size);
-        segment_gt_label=mode(gt(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
-        label_image(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=segment_gt_label;
+% Loop through each segment size
+for idx_current = 1:length(segments_vector)
+   
+    close all;
+    
+    tic;  % Start timing for each segment size
+    segment_size = segments_vector(idx_current);
+    segments = 3000 / segment_size
+    idx = zeros(3000);  % Pre-allocate idx for the maximum required size
 
-        feature_R(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(RGB_R(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
-        feature_G(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(RGB_G(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
-        feature_B(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(RGB_B(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
-        feature_nDSM(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(nDSM(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
-        feature_IR(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(IR(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
 
+    for i=1:segments
+        for j=1:segments
+%             idx(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=i*j*ones(segment_size);
+%             segment_gt_label=mode(gt(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+%             label_image(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=segment_gt_label;
+% 
+%             feature_R(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(RGB_R(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+%             feature_G(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(RGB_G(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+%             feature_B(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(RGB_B(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+%             feature_nDSM(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(nDSM(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+%             feature_IR(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size)=mean(IR(((i-1)*segment_size)+1:i*segment_size,((j-1)*segment_size)+1:j*segment_size),'all');
+            x_start = (i-1)*segment_size + 1;
+            x_end = i * segment_size;
+            y_start = (j-1)*segment_size + 1;
+            y_end = j * segment_size;
+            idx(x_start:x_end, y_start:y_end) = i*j;  % Assign segment ID
+
+
+            % Directly use ground truth data without storing indices
+            segment_gt_label = mode(gt(x_start:x_end, y_start:y_end), 'all');
+            label_image(x_start:x_end, y_start:y_end) = segment_gt_label;
+
+            % Calculate and store features
+            feature_R(x_start:x_end, y_start:y_end) = mean(RGB_R(x_start:x_end, y_start:y_end), 'all');
+            feature_G(x_start:x_end, y_start:y_end) = mean(RGB_G(x_start:x_end, y_start:y_end), 'all');
+            feature_B(x_start:x_end, y_start:y_end) = mean(RGB_B(x_start:x_end, y_start:y_end), 'all');
+            feature_nDSM(x_start:x_end, y_start:y_end) = mean(nDSM(x_start:x_end, y_start:y_end), 'all');
+            feature_IR(x_start:x_end, y_start:y_end) = mean(IR(x_start:x_end, y_start:y_end), 'all');
+        end
     end
+    
+    idx=reshape(idx,[],1);
+
+    mask=reshape(idx,3000,3000);
+
+    figure
+    imshow(mask,[])
+    title(['chessboard segmentation mask with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
+
+    figure
+    bmask = boundarymask(mask);
+    imshow(imoverlay(RGB,bmask,'cyan'))
+    title(['chessboard segments on RGB image with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
+
+    imp_surf = label_image == 1;
+    building = label_image == 2;
+    low_veg  = label_image == 3;
+    tree     = label_image == 4;
+    car      = label_image == 5;
+    clutter  = label_image == 6;
+
+    r = imp_surf | car | clutter;
+    g = imp_surf | low_veg | tree | car;
+    b = imp_surf | building | low_veg;
+
+    RGB_label_image = cat(3,r,g,b) * 255;
+
+    feature_RGB=ones(3000,3000,3);
+    feature_RGB(:,:,1)=feature_R;
+    feature_RGB(:,:,2)=feature_G;
+    feature_RGB(:,:,3)=feature_B;
+    
+    %% determine runtime
+    times(idx_current) = toc;  % End timing and capture the elapsed time
+    disp(['Elapsed Time: ', num2str(times(idx_current)), ' seconds']);
+    
+    
+    %% plot images
+    figure
+    imshow(feature_RGB)
+    title(['chessboard RGB features with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
+
+    figure
+    imshow(RGB_label_image)
+    title(['chessboard labeled image with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
+
+    %% determine accuracy
+    result_label_assignments = 1*imp_surf + 2*building + 3*low_veg + 4*tree + 5*car + 6*clutter;
+    correctly_identified_labels = result_label_assignments == gt;
+    accuracies(idx_current) = sum(correctly_identified_labels, "all") / (3000*3000)
 end
-idx=reshape(idx,[],1);
 
-mask=reshape(idx,3000,3000);
+%% Plot accuracies and times
+f = figure;
+yyaxis left;
+plot(segments_vector, accuracies*100, '-o');
+ylabel('Accuracy [%]');
+ylim([0 100]);  % Set y-axis limits from 0 to 100
 
-figure
-imshow(mask,[])
-title(['chessboard segmentation mask with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
 
-figure
-bmask = boundarymask(mask);
-imshow(imoverlay(RGB,bmask,'cyan'))
-title(['chessboard segments on RGB image with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
+yyaxis right;
+plot(segments_vector, times, '-o');
+ylabel('Time (s)');
+ylim([0 max(times)]);  % Set y-axis limits from 0 to the maximum time
 
-imp_surf = label_image == 1;
-building = label_image == 2;
-low_veg  = label_image == 3;
-tree     = label_image == 4;
-car      = label_image == 5;
-clutter  = label_image == 6;
 
-r = imp_surf | car | clutter;
-g = imp_surf | low_veg | tree | car;
-b = imp_surf | building | low_veg;
+xlabel('Segment Size');
+title('Accuracy and Runtime for Various Segment Sizes');
+legend('Accuracy', 'Runtime', 'Location', 'best');
+saveas(f, ['results_chessboard/accuracy_runtime_' datestr(now, 'yyyy-mm-dd_HHMMSS') '.png']);
 
-RGB_label_image = cat(3,r,g,b) * 255;
-
-feature_RGB=ones(3000,3000,3);
-feature_RGB(:,:,1)=feature_R;
-feature_RGB(:,:,2)=feature_G;
-feature_RGB(:,:,3)=feature_B;
-
-figure
-imshow(feature_RGB)
-title(['chessboard RGB features with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
-
-figure
-imshow(RGB_label_image)
-title(['chessboard labeled image with segment width=' ,num2str(segment_size), ' pix total segments= ',num2str(segments*segments)])
-
-%% determine accuracy
-result_label_assignments = 1*imp_surf + 2*building + 3*low_veg + 4*tree + 5*car + 6*clutter;
-correctly_identified_labels = result_label_assignments == gt;
-accuracy = sum(correctly_identified_labels, "all") / (3000*3000)
-
-%% determine runtime
-elapsedTime = toc;  % End timing and capture the elapsed time
-disp(['Elapsed Time: ', num2str(elapsedTime), ' seconds']);
 
